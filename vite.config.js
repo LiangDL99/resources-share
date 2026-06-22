e8221a6078bca2fd859a805fb2a542220beeb8a8
LiangDL99 <liangdl99@gmail.com>
2026-06-22 20:30:16 +0800

    修复静态资源路径问题，支持直接打开本地 HTML 文件

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,14 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'docs'
-  }
+    outDir: 'docs',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
+  base: './'
 })
